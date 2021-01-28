6806de01ea3f8cc9e64754d7c718d41932e96ed0
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2021-01-28 18:31:05 +0100

    Initial Build w/ UART and internal LED

---
+++ Smart_Dom/src/main.c
@@ -0,0 +1,57 @@
+#include <zephyr.h>
+#include <logging/log.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   500
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#else
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led0 devicetree alias is not defined"
+#define LED0	""
+#define PIN	0
+#define FLAGS	0
+#endif
+
+
+LOG_MODULE_REGISTER(main);
+void main(void)
+{
+  bool smoke = false;
+  uint8_t outside_light = 100;
+  uint8_t inside_light = 0;
+  uint8_t blinds_pos = 0;
+  
+	printk("WELCOME, ALL SYSTEMS ARE OPERATIONAL\n");
+
+  const struct device *dev;
+	bool led_is_on = true;
+	int ret;
+
+	dev = device_get_binding(LED0);
+	if (dev == NULL) {
+		return;
+	}
+
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		gpio_pin_set(dev, PIN, (int)led_is_on);
+		led_is_on = !led_is_on;
+		k_msleep(SLEEP_TIME_MS);
+    printk("%i\n",PIN);
+    printk("test\n");
+	}
+}
